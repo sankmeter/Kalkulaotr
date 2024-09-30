ee841dd1e8745c620c8317af93ef313e6a860aa4
sankmeter <danik67you@gmail.com>
2024-09-30 17:33:35 +0200

    list last to first

---
+++ list4.py
@@ -0,0 +1,9 @@
+def last_to_first(lst):
+    if len(lst) <= 1:
+        return lst
+    last_element = lst.pop()
+    lst.insert(0, last_element)
+    return lst
+moy_list = [1,10,23,5]
+resultat = last_to_first(moy_list)
+print(resultat)
